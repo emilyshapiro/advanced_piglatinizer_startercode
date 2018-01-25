99f6d438c9d18f14d2e97daf2a4411185e236f59
Emily Shapiro <emily@scripted.org>
2018-01-25 00:50:23 +0000

    one word pig latinizer

--- script.js
+++ script.js
@@ -6,33 +6,74 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
 
 $(document).ready(function() {
+function startsWithVowel(word){
+    $("#pig").append(word + "yay");
+    }
+  
+function startsWithConsonant(word){
+    
+     
+     $("#pig").append(word.charAt(1) + word.substring(0)+ "ay");
+    }
+    
+    
+    $("button").click(function(){
+        var word =$("input").val();
+         $("#pig").empty();
+  
+   if (word.charAt(0) === "a" || word.charAt(0) === "e" || word.charAt(0) === "i" || word.charAt(0) === "o"|| word.charAt(0) === "u"){
+            startsWithVowel(word);
+      
+       }
+    else {
+        startsWithConsonant(word);
+    }
+    
+    
+    });
 
+
+
+
+/*----------------------- Exercise 4-----------------------*/
+// Moves the first consonant to the end of the word, appends "ay" to the end of the word,
+// and returns the word.
+// (coding --> odingcay)
+//function convertWordWithConsonant(word) {
+
+//}
+
+
+    
+    
+//Math.random();
     // This function should return true if the input word starts with a vowel,
     // otherwise it should return false.
-    function wordStartsWithVowel(word) {
+    //function wordStartsWithVowel(word) {
+        
 
-    }
+   // }
 
     // Appends "way" to the end of the word
-    function wordStartsWithVowelTransformation(word) {
+   // function wordStartsWithVowelTransformation(word) {
 
-    }
+    //}
 
     // Moves the first consonant to the end of the word and appends "ay" to the end of the word
-    function wordStartsWithConsonantTransformation(word) {
+    //function wordStartsWithConsonantTransformation(word) {
 
-    }
+    //}
 
     // If the word starts with a vowel return the result the wordStartsWithVowelTransformation function.
     // Otherwise return the result of the wordStartsWithVowelTransformation.
-    function toPigLatinWord(word) {
+    //function toPigLatinWord(word) {
 
-    }
+    //}
 
     //Iterates through all the words in the sentence and transforms each word to pig latin
-    function toPigLatinSentence(sentence) {
+    //function toPigLatinSentence(sentence) {
 
-    }
+    //}
 
     // Create a click handler that takes in the value from the input box.
         // Gets a sentence from an input
